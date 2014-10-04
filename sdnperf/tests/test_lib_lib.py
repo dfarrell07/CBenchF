c1ea54537e9efd3e80121191236f772dedc6c16c
Daniel Farrell <dfarrell@redhat.com>
2014-10-04 00:32:04 -0400

    Refactored location of tests to be more standard
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- sdnperf/tests/test_lib_lib.py
+++ sdnperf/tests/test_lib_lib.py
@@ -3,14 +3,21 @@
 import unittest
 import logging
 
-import sdnperf.lib.lib as lib
+try:
+    import yaml
+except ImportError, err:
+    import sys
+    sys.stderr.write("ERROR: {}. Try installing python-yaml.\n".format(err))
+    raise
+
+import lib.lib as lib
 
 
 class TestGetLogger(unittest.TestCase):
 
     """Tests for the lib method that builds loggers."""
 
-    @unittest.expectedFailure
+    #@unittest.expectedFailure
     def test_type(self):
         """Confirm that the object returned is of type Logger."""
         logger = lib.get_logger()
@@ -21,7 +28,7 @@ class TestGetConfig(unittest.TestCase):
 
     """Tests for the lib method that loads the general program config."""
 
-    @unittest.expectedFailure
+    #@unittest.expectedFailure
     def test_type(self):
         """Confirm that the object returned is of type Logger."""
         config = lib.get_config()
