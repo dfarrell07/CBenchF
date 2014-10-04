9c7c6f71e517ec5e01fd1e858422fa11d37a1a9c
Daniel Farrell <dfarrell@redhat.com>
2014-10-03 23:50:58 -0400

    Added failing test (TDD) for lib's get_config fn
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- sdnperf/lib/tests/test_lib.py
+++ sdnperf/lib/tests/test_lib.py
@@ -15,3 +15,14 @@ class TestGetLogger(unittest.TestCase):
         """Confirm that the object returned is of type Logger."""
         logger = lib.get_logger()
         assert type(logger) is logging.Logger
+
+
+class TestGetConfig(unittest.TestCase):
+
+    """Tests for the lib method that loads the general program config."""
+
+    @unittest.expectedFailure
+    def test_type(self):
+        """Confirm that the object returned is of type Logger."""
+        config = lib.get_config()
+        assert type(config) is dict
