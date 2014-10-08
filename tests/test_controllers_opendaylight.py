1b258c64c123f7408df1d3093aecc5e3177ca961
Daniel Farrell <dfarrell@redhat.com>
2014-10-08 10:45:29 -0400

    Added API entry point for starting ODL
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ tests/test_controllers_opendaylight.py
@@ -0,0 +1,27 @@
+"""Tests for the module that abstracts the OpenDaylight controller."""
+
+import unittest
+
+import sdnperf.controllers.opendaylight as odl_mod
+
+
+class TestInit(unittest.TestCase):
+
+    """Test building the ODL abstraction."""
+
+    def test_basic(self):
+        """Test a basic, all default construction."""
+        odl_mod.OpenDaylight()
+
+
+class TestStart(unittest.TestCase):
+
+    """Test starting an OpenDaylight controller instance"""
+
+    def setUp(self):
+        """Build an ODL abstraction."""
+        self.odl = odl_mod.OpenDaylight()
+
+    def test_present(self):
+        """Confirm that the start method is present. Required API method."""
+        hasattr(self.odl, "start") and callable(getattr(self.odl, "start"))
