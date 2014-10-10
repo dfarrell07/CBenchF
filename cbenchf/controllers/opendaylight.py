ee66da5310a5540a53a56211bccaa98aa917d8c4
Daniel Farrell <dfarrell@redhat.com>
2014-10-10 12:39:09 -0400

    Added details about start/stop ODL API stubs
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- cbenchf/controllers/opendaylight.py
+++ cbenchf/controllers/opendaylight.py
@@ -12,9 +12,27 @@ class OpenDaylight(object):
         self.logger = lib.get_logger()
 
     def start(self):
-        """API entry point for starting an ODL controller instance."""
+        """API entry point for starting an ODL controller instance.
+
+        Initially, this might be done via WCBench. Would be better
+        to move to something like Docker in the mid-term.
+
+        See: https://github.com/dfarrell07/CBenchF/issues/6
+
+        TODO: This is a stub.
+
+        """
         pass
 
     def stop(self):
-        """API entry point for stopping an ODL controller instance."""
+        """API entry point for stopping an ODL controller instance.
+
+        Initially, this might be done via WCBench. Would be better
+        to move to something like Docker in the mid-term.
+
+        See: https://github.com/dfarrell07/CBenchF/issues/6
+
+        TODO: This is a stub.
+
+        """
         pass
